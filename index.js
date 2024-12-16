2d77696e9a5d4fc777bbfa013af755f30f4e81eb
Sabid Farhan <sfttanim@gmail.com>
2024-12-16 20:27:20 +0600

    dashboard database added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -17,7 +18,7 @@ app.use(
 );
 
 // MongoDB
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@cluster0.9sxzsr9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -30,14 +31,51 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-
     // Database Collections
     const usersCollections = client.db("Portfolio").collection("users");
+    const contactCollections = client.db("Portfolio").collection("contact");
+    const blogCollections = client.db("Portfolio").collection("blogs");
 
+    // Token Added
+    // JWT Related API (Pass the token in client site)
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+      res.send({ token });
+    });
+
+    // Token Verify
+    const verifyToken = (req, res, next) => {
+      // req.headers.authorization comes from client site like useAxiosSecure file
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Unauthorized Access!" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: "Unauthorized Access!" });
+        }
+        res.decoded = decoded;
+        next();
+      });
+    };
+
+    app.get("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      if (!email) {
+        return res.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollections.findOne(query);
+      if (user?.role === "admin") {
+        const isAdmin = user?.role === "admin";
+        res.send({ admin: isAdmin });
+      } else {
+        return res.send({ admin: false });
+      }
+    });
+
+    // USERS
     app.get("/users", async (req, res) => {
       const result = await usersCollections.find().toArray();
       res.send(result);
@@ -55,6 +93,86 @@ async function run() {
         });
       }
       const result = await usersCollections.insertOne(userInfo);
+      res.send(result);
+    });
+
+    // CONTACT
+    app.get("/contact", async (req, res) => {
+      const result = await contactCollections.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/contact", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const filter = { email: req.body.email };
+      const existingUser = await contactCollections.findOne(filter);
+      const options = { upsert: true };
+      if (existingUser) {
+        const updateDoc = {
+          $push: {
+            message: { $each: data.message },
+          },
+        };
+        const result = await contactCollections.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        return res.send(result);
+      }
+      const result = await contactCollections.insertOne(data);
+    });
+
+    // Blogs
+    app.get("/blogs", async (req, res) => {
+      const result = await blogCollections.find().toArray();
+      res.send(result);
+    });
+
+    // Blog: Fine a single one
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await blogCollections.findOne(filter);
+      res.send(result);
+    });
+
+    // Blog: Update
+    app.post("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          title: data.title,
+          description: data.description,
+          timeToRead: data.timeToRead,
+          category: data.category,
+        },
+      };
+      const result = await blogCollections.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // Blog: Delete
+    app.delete("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await blogCollections.deleteOne(filter);
+      res.send(result);
+    });
+
+    // Blog: New Add
+    app.post("/blog", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await blogCollections.insertOne(data);
       res.send(result);
     });
   } finally {
